5655b2ffaff368783bbf4ad6e1ef171bbadad07b
maciej_lapa <maclapa95@gmail.com>
2025-08-29 18:16:53 +0200

    Next tasks

---
+++ 30DaysOfJS/2666-allow-one-function-call.js
@@ -0,0 +1,32 @@
+// Given a function fn, return a new function that is identical to the original 
+// function except that it ensures fn is called at most once.
+
+// The first time the returned function is called, it should return the same result as fn.
+// Every subsequent time it is called, it should return undefined.
+ 
+
+// Example 1:
+
+// Input: fn = (a,b,c) => (a + b + c), calls = [[1,2,3],[2,3,6]]
+// Output: [{"calls":1,"value":6}]
+// Explanation:
+// const onceFn = once(fn);
+// onceFn(1, 2, 3); // 6
+// onceFn(2, 3, 6); // undefined, fn was not called
+
+fn = (a,b,c) => (a + b + c)
+
+function once(fn){
+    let runned = false
+    return function(...args){
+        if(!runned){
+            runned = true
+            return fn(...args)
+        }
+        return undefined
+    }
+}
+
+const onceFn = once(fn);
+console.log(onceFn(1, 2, 3)); // 6
+console.log(onceFn(2, 3, 6));
